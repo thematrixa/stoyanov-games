5dc5e810b3ce901015a6ca2cd3bcffe742e81624
ivegor <ivelin.ivanov@nevexis.com>
2019-07-10 17:41:52 +0300

    added respectfull fields to models

--- stoyanovgames/src/main/java/com/gorchovski/stoyanovgames/model/Category.java
+++ stoyanovgames/src/main/java/com/gorchovski/stoyanovgames/model/Category.java
@@ -2,4 +2,12 @@ package com.gorchovski.stoyanovgames.model;
 
 public class Category {
 
+    private Integer id ;
+    private String name ;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
 }
--- stoyanovgames/src/main/java/com/gorchovski/stoyanovgames/model/Order.java
+++ stoyanovgames/src/main/java/com/gorchovski/stoyanovgames/model/Order.java
@@ -1,5 +1,77 @@
 package com.gorchovski.stoyanovgames.model;
 
-public class Order {
+import java.util.ArrayList;
 
+public class Order {
+	private String id;
+	private String userId;
+	private String name;
+	private String address;
+	private String date;
+	private String phone;
+	private String total;
+	private ArrayList<Product> products;
+	private Boolean showProducts;
+	private String status;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getTotal() {
+		return total;
+	}
+	public void setTotal(String total) {
+		this.total = total;
+	}
+	public ArrayList<Product> getProducts() {
+		return products;
+	}
+	public void setProducts(ArrayList<Product> products) {
+		this.products = products;
+	}
+	public Boolean getShowProducts() {
+		return showProducts;
+	}
+	public void setShowProducts(Boolean showProducts) {
+		this.showProducts = showProducts;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
 }
